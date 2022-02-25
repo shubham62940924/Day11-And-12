dbac4adfc7fede8c97ed6b4ea5d683d955a69068
first_name <ss329222@gmail.com>
2022-02-25 16:54:46 +0530

    Added:StockClass

---
+++ src/com/blz/practice/Stock.java
@@ -0,0 +1,46 @@
+package com.blz.practice;
+
+public class Stock {
+	// instance variables(declared inside class only)
+	private String stockNames;
+	private Double numberOfShare;
+	private Double sharePrice;
+
+	public Stock() {
+		super();
+	}
+
+	// Parameterized constructor
+	public Stock(String stockNames, Double numOfShare, Double sharePrice) {
+		super();
+		this.stockNames = stockNames;
+		this.numberOfShare = numOfShare;
+		this.sharePrice = sharePrice;
+	}
+
+	// Apply getters and setters
+	public String getStockNames() {
+		return stockNames;
+	}
+
+	public void setStockNames(String stockNames) {
+		this.stockNames = stockNames;
+	}
+
+	public Double getNumOfShare() {
+		return numberOfShare;
+	}
+
+	public void setNumOfShare(Double numOfShare) {
+		this.numberOfShare = numOfShare;
+	}
+
+	public Double getSharePrice() {
+		return sharePrice;
+	}
+
+	public void setSharePrice(Double sharePrice) {
+		this.sharePrice = sharePrice;
+	}
+
+}
